7d975eb04b4c86343498d7944ed24d08f3d1e91b
Tyrek <rayslinger244@gmail.com>
2021-07-31 05:20:56 -0500

    prints products in Inventory only once

--- src/Inventory.java
+++ src/Inventory.java
@@ -13,20 +13,20 @@ public class Inventory {
     public void addProduct(Product newProduct) {
         inventoryArray.add(newProduct);
         int count;
-        if (inventoryCountMap.get(newProduct.getProductCode()) == null) {
+        if (inventoryCountMap.get(newProduct.getProductID()) == null) {
             count = 1;
         } else {
-            count = (int) inventoryCountMap.get(newProduct.getProductCode());
+            count = (int) inventoryCountMap.get(newProduct.getProductID());
             count = count + 1;
         }
-        inventoryCountMap.put(newProduct.getProductCode(), count);
+        inventoryCountMap.put(newProduct.getProductID(), count);
     }
 
     public Product removeProduct(String newProductCode) {
         Iterator iterator = inventoryArray.iterator();
         while (iterator.hasNext()){
             Product product = (Product) iterator.next();
-            if (newProductCode.equals(product.getProductCode())) {
+            if (newProductCode.equals(product.getProductID())) {
                 iterator.remove();
                 int count = (int) inventoryCountMap.get(newProductCode);
                 count = count - 1;
@@ -38,12 +38,18 @@ public class Inventory {
     }
 
     public void printInventory() {
-        Iterator iterator = inventoryArray.iterator();
+        Iterator iterator = inventoryCountMap.keySet().iterator();
         while (iterator.hasNext()) {
-            Product product = (Product) iterator.next();
-            System.out.println("——————————————————");
-            product.printProductDetails();
-            System.out.println("\nStock: " + inventoryCountMap.get(product.getProductCode()));
+            String productType = (String) iterator.next();
+            for (int i = 0; i < inventoryArray.size(); i++) {
+                Product product = (Product) inventoryArray.get(i);
+                if (product.getProductID().equals(productType)) {
+                    System.out.println("——————————————————");
+                    product.printProductDetails();
+                    System.out.println("\nStock: " + inventoryCountMap.get(product.getProductID()) + "");
+                    break;
+                }
+            }
         }
         System.out.println("——————————————————");
     }
